004b451e30c9ede26e0caa1a910b953ba8303b25
Olga Parfenova <work.parfenova@gmail.com>
2023-01-31 14:23:22 +0300

    The first problem is solved

---
+++ script.js
@@ -0,0 +1,20 @@
+// const person = new Object({
+//     name: 'Dan',
+//     age: 36,
+//     greet: function() {
+//         console.log('Greetings!')
+//     }
+// });
+//
+// Object.prototype.sayHello = function () {
+//     console.log('Hello!')
+// }
+//
+// const masha = Object.create(person);
+//
+// masha.name = "Mary";
+//
+// const str = new String('This is a string');
+//
+// console.log(str.sayHello());
+
